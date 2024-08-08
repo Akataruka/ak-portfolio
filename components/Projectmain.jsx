7db966b6f1a28cfcc2cc0ce5e7319202fed9d236
Akataruka <121378361+Akataruka@users.noreply.github.com>
2024-08-08 22:23:11 +0530

    edit5

--- components/Projectmain.jsx
+++ components/Projectmain.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Projectbox from "./projectbox";
+import Projectbox from "./Projectbox";
 import { useState } from "react";
 
 const projectitems = [
